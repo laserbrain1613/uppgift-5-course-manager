bbcef60c6959d317492a53d18b9b8d9620506099
BuildTools <unconfigured@null.spigotmc.org>
2020-08-07 09:13:01 +0200

    Initial coding for Student and Course classes

--- src/main/java/se/lexicon/course_manager_assignment/model/Course.java
+++ src/main/java/se/lexicon/course_manager_assignment/model/Course.java
@@ -1,5 +1,75 @@
 package se.lexicon.course_manager_assignment.model;
 
-public class Course {
+import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Objects;
+
+public class Course {
+    private final int id; // Unique attribute, should be set through constructor with CourseSequencer.class in CourseCollectionRepository.class
+    private String courseName; // representing the name of a Course object (like “Java advanced”)
+    private LocalDate startDate; // of type LocalDate defines the start date of a course object.
+    private int weekDuration; // of type int or Integer is a description of the length of a course
+    private Collection<Student> students; // representing all objects of Student.class that is enrolled to this course.
+
+    public Course(int id) { // minimum requirement is to pass in id through constructor.
+        this.id = CourseSequencer.nextCourseId();
+    }
+
+    public Course(int id, String courseName, LocalDate startDate, int weekDuration, Collection<Student> students) {
+        this.id = CourseSequencer.nextCourseId();
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.students = students;
+    }
+
+    public int getId() { return id; }
+    public String getCourseName() { return courseName; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+    public LocalDate getStartDate() { return startDate; }
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
+    public Collection<Student> getStudents() { return students; }
+    public void setStudents(Collection<Student> students) { this.students = students; }
+
+    // should be used to add a Student.class object to Collection<Student> students. Make sure you avoid adding a
+    // duplicate or null into the Collection. Should return true when student was successfully added, otherwise false.
+    public boolean enrollStudent(Student student) {
+        return false;
+    }
+
+    // should be used to remove a Student.class object from Collection<Student> students. Returns true when the Student
+    // object was successfully removed.
+    public boolean unenrollStudent(Student student) {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) { // needed internally in Collections
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id &&
+                weekDuration == course.weekDuration &&
+                courseName.equals(course.courseName) &&
+                startDate.equals(course.startDate) &&
+                students.equals(course.students);
+    }
+
+    @Override
+    public int hashCode() { // needed internally in Collections
+        return Objects.hash(id, courseName, startDate, weekDuration, students);
+    }
+
+    @Override
+    public String toString() { // for debugging purposes
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", students=" + students +
+                '}';
+    }
 }
--- src/main/java/se/lexicon/course_manager_assignment/model/Student.java
+++ src/main/java/se/lexicon/course_manager_assignment/model/Student.java
@@ -1,5 +1,57 @@
 package se.lexicon.course_manager_assignment.model;
 
-public class Student {
+import se.lexicon.course_manager_assignment.data.sequencers.StudentSequencer;
 
+import java.util.Objects;
+
+public class Student {
+    private final int id; // Unique attribute, should be set through constructor with StudentSequencer.class
+    private String name; // representing the full name of the Student.
+    private String email; // unique attribute representing the Student’s email.
+    private String address; // String representation of the Student’s address.
+
+    public Student(int id) {
+        this.id = StudentSequencer.nextStudentId(); // minimum requirement is to pass in id through constructor
+    }
+
+    public Student(int id, String name, String email, String address) {
+        this.id = StudentSequencer.nextStudentId();
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    @Override
+    public boolean equals(Object o) { // needed internally in Collections
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id &&
+                name.equals(student.name) &&
+                email.equals(student.email) &&
+                address.equals(student.address);
+    }
+
+    @Override
+    public int hashCode() { // needed internally in Collections
+        return Objects.hash(id, name, email, address);
+    }
+
+    @Override
+    public String toString() { // for debugging purpose
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
